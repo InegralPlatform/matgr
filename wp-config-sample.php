0f9cb0ddcd7a36e7642cf72bf8850271bbb387f1
InegralPlatform <44678853+InegralPlatform@users.noreply.github.com>
2019-04-28 19:41:07 +0200

    update database

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -22,17 +22,17 @@
  /** The name of the database for WordPress */
  define('WP_CACHE', true);
  define( 'WPCACHEHOME', 'C:\xampp\htdocs\DO\wordpress\wp-content\plugins\wp-super-cache/' );
- define( 'DB_NAME', 'hi23jmaq56gpjzur' );
+ define( 'DB_NAME', 'rgrqlwwoxwsglmiv' );
 
  /** MySQL database username */
- define( 'DB_USER', 'a60x3tpg6k616j16' );
+ define( 'DB_USER', 'q3a0vr1q5yulgjqf' );
 
  /** MySQL database password */
- define( 'DB_PASSWORD', '	enngs9wxivshvg3h' );
+ define( 'DB_PASSWORD', 'j5uvpnawf4hy6v35' );
 
 
  /** MySQL hostname */
- define( 'DB_HOST', 'icopoghru9oezxh8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com' );
+ define( 'DB_HOST', 'wm63be5w8m7gs25a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com' );
 
 
 
