1e692647e2a41886fa30177ef969f4cf640af33b
abobakr8911 <abubakr8911@gmail.com>
2019-05-04 08:50:47 +0200

    Define uploads pleaseeeeeeeeeeeee

--- wp-config.php
+++ wp-config.php
@@ -1,6 +1,6 @@
 <?php
 
-
+define( 'UPLOADS', 'wp-content/'.'uploads' );
 // BEGIN iThemes Security - Do not modify or remove this line
 // iThemes Security Config Details: 2
 define( 'DISALLOW_FILE_EDIT', true ); // Disable File Editor - Security > Settings > WordPress Tweaks > File Editor
